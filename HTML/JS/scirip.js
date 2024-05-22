1eb5dba2b5eb15f139a9567b367936b4ca81e447
Nguyễn Hữu Duy <123649327+noblesse004@users.noreply.github.com>
2024-05-22 21:16:28 +0700

    Add files via upload

---
+++ HTML/JS/scirip.js
@@ -0,0 +1,6 @@
+let textFruit : HTMLElement = document.getElementById('TxtFruit');
+let btnSearch = document.getElementById('btnSearch');
+let pNotify = document.getElementById('pNotify');
+let ingView = document.getElementById('ingView');
+
+let position = text.search("lemon");
